c28e7ce4a30926543facf35f24563ccf7a7cf3e9
D0A1V2I3D <itIsMeDavid@protonmail.com>
2023-01-16 14:23:52 +0100

    gui finish

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fmt::format;
+use std::{fmt::format, thread::JoinHandle};
 
 use eframe::egui::{self, TextBuffer};
-use reqwest::header::{HeaderMap, HeaderName};
+use reqwest::{header::{HeaderMap, HeaderName}, Method};
+use serde::{Deserialize, Serialize};
+use tracing::Value;
 
 fn main() {
     tracing_subscriber::fmt::init();
@@ -15,7 +17,7 @@ fn main() {
     eframe::run_native("Request spammer", options, Box::new(|_cc| {Box::new(App::default())}));
 }
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, Clone, Copy, Deserialize, Serialize)]
 enum Methods {
     Get,
     Post,
@@ -28,13 +30,38 @@ enum Methods {
     Trace,
 }
 
+#[derive(Serialize, Deserialize, Clone)]
 struct App {
+    #[serde(with = "http_serde::header_map")]
     headers: HeaderMap,
     body: String,
     spamming: bool,
     method: Methods,
     hname: String,
     hvalue: String,
+    threads: u32,
+    url: String,
+}
+
+impl App {
+    fn spam(val: String) {
+        let state: App = serde_json::from_str(val.as_str()).unwrap();
+        let client = reqwest::blocking::Client::new();
+        let method = match state.method {
+            Methods::Get => Method::GET,
+            Methods::Post => Method::POST,
+            Methods::Put => Method::PUT,
+            Methods::Delete => Method::DELETE,
+            Methods::Head => Method::HEAD,
+            Methods::Options => Method::OPTIONS,
+            Methods::Connect => Method::CONNECT,
+            Methods::Patch => Method::PATCH,
+            Methods::Trace => Method::TRACE,
+        };
+        loop {
+            client.request(method.clone(), &state.url).headers(state.headers.clone()).body(state.body.clone()).send();
+        }
+    }
 }
 
 impl Default for App {
@@ -46,6 +73,8 @@ impl Default for App {
             method: Methods::Get,
             hname: "".to_string(),
             hvalue: "".to_string(),
+            threads: 10,
+            url: "example.org".to_string()
         }
     }
 }
@@ -56,11 +85,17 @@ impl eframe::App for App {
             ui.heading("FreeShit request spammer");
             ui.vertical(|ui| {
                 if ui.button("SPAM").clicked() {
-                    //
+                    let mut vec: Vec<JoinHandle<&()>> = vec![];
+                    for i in 0..self.threads.clone() {
+                        std::thread::spawn(|| {App::spam(serde_json::to_string(&self.clone()).unwrap())});
+                    }
                 }
+                ui.add(egui::Slider::new(&mut self.threads, 1..=500).text("Threads"));
+                ui.label("Url");
+                ui.text_edit_singleline(&mut self.url);
                 egui::ComboBox::from_label("Method")
                 .selected_text(format!("{:?}", self.method))
-                .show_ui(ui, |ui| {
+                    .show_ui(ui, |ui| {
                     ui.selectable_value(&mut self.method, Methods::Get, "Get");
                     ui.selectable_value(&mut self.method, Methods::Post, "Post");
                     ui.selectable_value(&mut self.method, Methods::Put, "Put");
